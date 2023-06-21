368c2271450cf4e433940f1a9995abfcfcbde339
angel zambrano <angeldzambrano99@gmail.com>
2023-06-21 15:55:24 -0700

    added starter project

---
+++ meows/src/main/java/com/meowsic/meows/MeowsApplication.java
@@ -0,0 +1,13 @@
+package com.meowsic.meows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowsApplication.class, args);
+	}
+
+}
