9510e3bac79dd2af3f9a676bb6b35fa34e7572d8
angel zambrano <angeldzambrano99@gmail.com>
2023-06-21 17:50:10 -0700

    created postdto and added validation

---
+++ meows/src/main/java/com/meowsic/meows/payload/PostDto.java
@@ -0,0 +1,20 @@
+package com.meowsic.meows.payload;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private Long id;
+
+    @NotEmpty
+    private Long spot_id;
+
+    @NotEmpty
+    @Size(min = 10, message = "Post description should have at least 10 characters")
+    private String description;
+
+
+}
