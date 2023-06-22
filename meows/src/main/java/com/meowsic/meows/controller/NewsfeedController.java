1c18767f152c3bf42557ebdbcb1c2525bf285ecb
angel zambrano <angeldzambrano99@gmail.com>
2023-06-22 01:05:16 -0700

    added the getting all items

--- meows/src/main/java/com/meowsic/meows/controller/NewsfeedController.java
+++ meows/src/main/java/com/meowsic/meows/controller/NewsfeedController.java
@@ -1,14 +1,13 @@
 package com.meowsic.meows.controller;
 
 import com.meowsic.meows.payload.PostDto;
+import com.meowsic.meows.payload.PostResponse;
 import com.meowsic.meows.service.NewsfeedService;
+import com.meowsic.meows.utils.AppConstants;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/newsfeed")
@@ -27,4 +26,19 @@ public class NewsfeedController {
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto) {
         return new ResponseEntity<>(newsfeedService.createPost(postDto), HttpStatus.CREATED);
     }
+
+    // getting all of the posts
+    @GetMapping
+    public PostResponse getAllPost(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value ="sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ) {
+        return newsfeedService.getAllPosts(pageNo, pageSize,sortBy, sortDir);
+    }
+
+
+
+
 }
