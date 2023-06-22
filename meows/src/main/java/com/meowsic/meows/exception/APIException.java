782ca8514f8ae62269361c5fbcac1a953e5d4010
angel zambrano <angeldzambrano99@gmail.com>
2023-06-21 17:42:14 -0700

    added resource exception

--- meows/src/main/java/com/meowsic/meows/exception/APIException.java
+++ meows/src/main/java/com/meowsic/meows/exception/APIException.java
@@ -6,8 +6,15 @@ public class APIException extends RuntimeException{
     private HttpStatus status;
     private String message;
 
-    public APIException() {
-        
+    public APIException(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public APIException(String message, HttpStatus status, String message1) {
+        super(message);
+        this.status = status;
+        this.message = message1;
     }
 
 }
