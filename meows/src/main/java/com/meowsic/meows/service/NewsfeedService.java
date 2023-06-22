66d149b04e00b800c6bc386403de5abb1914fc27
angel zambrano <angeldzambrano99@gmail.com>
2023-06-22 01:02:45 -0700

    added get all service

--- meows/src/main/java/com/meowsic/meows/service/NewsfeedService.java
+++ meows/src/main/java/com/meowsic/meows/service/NewsfeedService.java
@@ -1,8 +1,9 @@
 package com.meowsic.meows.service;
 
 import com.meowsic.meows.payload.PostDto;
+import com.meowsic.meows.payload.PostResponse;
 
 public interface NewsfeedService {
     PostDto createPost(PostDto postDto);
-
+    PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
 }
--- meows/src/main/java/com/meowsic/meows/service/impl/NewsfeedServiceImpl.java
+++ meows/src/main/java/com/meowsic/meows/service/impl/NewsfeedServiceImpl.java
@@ -2,11 +2,18 @@ package com.meowsic.meows.service.impl;
 
 import com.meowsic.meows.entity.Post;
 import com.meowsic.meows.payload.PostDto;
+import com.meowsic.meows.payload.PostResponse;
 import com.meowsic.meows.repository.NewsfeedRepository;
 import com.meowsic.meows.service.NewsfeedService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 //import org.modelmapper.ModelMapper;
 @Service
@@ -26,6 +33,32 @@ public class NewsfeedServiceImpl implements NewsfeedService {
         Post newPost = newsfeedRepository.save(post);
         // convert entity to Dto
         PostDto postResponse = mapToDTO(newPost);
+        return postResponse;
+    }
+
+    @Override
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
+        PageRequest pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Post> posts = newsfeedRepository.findAll(pageable);
+
+        // get content for page object
+        List<Post> listOfPosts = posts.getContent();
+
+        List<PostDto> content = listOfPosts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+
+
         return postResponse;
     }
 
