c4cd1f00c61d07a6e882c1f3e13a06bdfe949eef
angel zambrano <angeldzambrano99@gmail.com>
2023-06-22 12:47:45 -0700

    adde user

---
+++ meows/src/main/java/com/meowsic/meows/entity/User.java
@@ -0,0 +1,29 @@
+package com.meowsic.meows.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
